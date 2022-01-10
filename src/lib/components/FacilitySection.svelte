97d328041112e4b2341f20af23f3141e1158268d
sonya-ninja <hello@sonya.ninja>
2022-01-10 06:53:25 +0200

    Start of facility section on explore page

---
+++ src/lib/components/FacilitySection.svelte
@@ -0,0 +1,55 @@
+<script>
+    import facility from "$lib/data/facility.json"
+</script>
+
+<section id="facility">
+    <div class="content">
+        <h2>Technology for change</h2>
+        <p class="subtitle">
+            All about our facility
+        </p>
+    </div>
+    <div class="gallery">
+        {#each facility.facilityFeatures as feature}
+            <div class="feature">
+                <h4>{feature.title}</h4>
+                <img src="{feature.image}" alt="description here">
+                <p>{feature.content}</p>
+            </div>
+        {/each}
+    </div>
+</section>
+
+<style lang="scss">
+    section {
+        border: 1px solid red;
+        padding: 3vw;
+        border: 1px solid red;
+        .content {
+            grid-column: 2;
+            grid-row: 1;
+            border: 1px solid red;
+            h2 {
+                margin-top: 0;
+            }
+        }
+        .gallery {
+            grid-column: 1;
+            grid-row: 1;
+            .feature {
+                h4 {
+                    position: absolute;
+                }
+                p {
+                    display: none;
+                }
+            }
+        }
+    }
+    @media (min-width: $mdScreen) {
+        section {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+        }
+    }
+</style>
--- src/routes/explore/index.svelte
+++ src/routes/explore/index.svelte
@@ -1,30 +1,35 @@
 <script>
     import Header from '$lib/components/Header.svelte';
+    import FacilitySection from "$lib/components/FacilitySection.svelte"
     import { slideFadeIn, slideFadeOut } from "$lib/animation/transition-slideFade";
 </script>
 
 
 <div class="pageWrap" in:slideFadeIn out:slideFadeOut>
-    <Header bgImage="https://res.cloudinary.com/committed-bodies/image/upload/f_auto,q_auto,t_pageHeader/v1631431623/gym/Gym-in-Benoni_m5uh6j.jpg">EXPLORE</Header>
-    <section id="facility">
-        <h2>Facility</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
-    </section>
+    <Header bgImage="https://res.cloudinary.com/committed-bodies/image/upload/f_auto,q_auto,t_pageHeader/v1631431623/gym/Gym-in-Benoni_m5uh6j.jpg">
+        EXPLORE
+    </Header>
+
+    <FacilitySection />
+
     <section id="trainers">
         <h2>Personal Trainers</h2>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
     </section>
+
     <section id="services">
         <h2>Services</h2>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
     </section>
+
 </div>
 
 
 
 <style lang="scss">
     section {
-        padding: $space2;
+        padding: 3vw;
         max-width: 100%;
+
     }
 </style>
