e9dfe1cba70a13b3b079bb30bdc309669e81bc35
sonya-ninja <hello@sonya.ninja>
2022-01-11 06:21:56 +0200

    Temporary trainers data for trainers section

---
+++ src/routes/facility/index.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Header from '$lib/components/Header.svelte';
+    import FacilitySection from "$lib/components/FacilitySection.svelte";
+    import { slideFadeIn, slideFadeOut } from "$lib/animation/transition-slideFade";
+</script>
+
+
+<div class="pageWrap" in:slideFadeIn out:slideFadeOut>
+    <Header bgImage="https://res.cloudinary.com/committed-bodies/image/upload/f_auto,q_auto,t_pageHeader/v1631431623/gym/Gym-in-Benoni_m5uh6j.jpg">
+        Facility
+    </Header>
+
+    <FacilitySection />
+
+</div>
