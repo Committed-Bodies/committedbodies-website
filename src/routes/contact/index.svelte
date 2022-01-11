cb3b4f60c146dca4d8a7df7f2273396fffef39a9
sonya-ninja <hello@sonya.ninja>
2022-01-11 19:41:07 +0200

    theme colors + styling for contact intro section

--- src/routes/contact/index.svelte
+++ src/routes/contact/index.svelte
@@ -13,9 +13,17 @@
         Contact
     </Header>
     <div class="contentWrap">
-        <h2>How can we help?</h2>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita quisquam repudiandae similique assumenda at. Laudantium, quaerat aliquid distinctio nesciunt, fugiat mollitia commodi, illum rerum veritatis nihil cumque eum hic voluptate!</p>
-        <p>Perferendis facere numquam assumenda ipsam, modi, quam eum vel nobis iure fugit blanditiis, autem architecto. Quisquam asperiores consectetur aspernatur, porro perspiciatis quidem! Maiores eligendi ad repellendus quas eius. Dolor, ipsum?</p>
+        
+        <div class="content">
+            <!-- Donavan - replace the content text in this section with props from a data file. See Trello card -->
+            <h2>How can we help?</h2>
+            <p class="subtitle h4">
+                Contact us
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita quisquam repudiandae similique assumenda at. Laudantium, quaerat aliquid distinctio nesciunt, fugiat mollitia commodi, illum rerum veritatis nihil cumque eum hic voluptate!</p>
+            <p>Perferendis facere numquam assumenda ipsam, modi, quam eum vel nobis iure fugit blanditiis, autem architecto. Quisquam asperiores consectetur aspernatur, porro perspiciatis quidem! Maiores eligendi ad repellendus quas eius. Dolor, ipsum?</p>
+        </div>
+        
         <form name="contact" method="POST" data-netlify="true">
             <p>
                 <label>Your Name: <input type="text" name="name" /></label>
@@ -36,8 +44,23 @@
 
 <style lang="scss">
     .contentWrap {
-        padding: $space4;
+        padding: 2rem;
         background-color: hsl(var(--neutralHS) var(--neutralL));
+        color: hsl(var(--onNeutral) / var(--onNeutralStrength0));
+        .content {
+            color: hsl(var(--onNeutral) / var(--onNeutralStrength0));
+            h2 {
+                margin-top: 0;
+				font-family: $font_boldEmphasis;
+				line-height: 1;
+				color: hsl(var(--onNeutral) / var(--onNeutralStrength3));
+			}
+			.subtitle {
+				font-family: $font_cursiveEmphasis;
+				margin-top: 1em;
+				color: hsl(var(--onNeutral) / var(--onNeutralStrength1));
+			}
+        }
         form {
             margin-top: $space4;
             background-color: #ffffff;
