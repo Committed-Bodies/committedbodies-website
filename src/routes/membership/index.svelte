e49e8077681927eb3f3e9513b8977cd2831d2b4d
Donavan Grobler <donavangrobler2@gmail.com>
2022-01-13 17:22:36 +0200

    Membership pages Catogories in config.yml

--- src/routes/membership/index.svelte
+++ src/routes/membership/index.svelte
@@ -5,42 +5,20 @@
 
 
 <div class="pageWrap" in:slideFadeIn out:slideFadeOut>
-    <Header>MEMBERSHIP</Header>
-    <section>
-        <h2>PLACEHOLDER</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
-    </section>
-    <section>
-        <h2>PLACEHOLDER</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
-    </section>
-    <section>
-        <h2>PLACEHOLDER</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
-    </section>
-    <section>
-        <h2>PLACEHOLDER</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
-    </section>
-    <section>
-        <h2>PLACEHOLDER</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
-    </section>
-    <section>
-        <h2>PLACEHOLDER</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
-    </section>
-    <section>
-        <h2>PLACEHOLDER</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
-    </section>
+    <Header bgImage="https://res.cloudinary.com/committed-bodies/image/upload/f_auto,q_auto,t_pageHeader/v1631431623/gym/Gym-in-Benoni_m5uh6j.jpg">
+        Membership
+    </Header>
+    <script src="https://CommittedBodiesFitnessCentresa.clubm.mobi/Portal/Home/clubmanager-integration.js"></script>
+<!-- svelte-ignore a11y-missing-attribute -->
+<iframe width="100%" height="1700" allowtransparency="true" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0" src="https://CommittedBodiesFitnessCentresa.clubm.mobi/Member/Joining.mvc"></iframe>
+                        
 </div>
 
 
 
-<style lang="scss">
+<!-- <style lang="scss">
     section {
         padding: $space2;
         max-width: 100%;
     }
-</style>
+</style> -->
---
+++ src/routes/membership/schedule.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Header from '$lib/components/Header.svelte';
+    import { slideFadeIn, slideFadeOut } from "$lib/animation/transition-slideFade";
+</script>
+
+<div class="pageWrap" in:slideFadeIn out:slideFadeOut>
+    <Header bgImage="https://res.cloudinary.com/committed-bodies/image/upload/f_auto,q_auto,t_pageHeader/v1631431623/gym/Gym-in-Benoni_m5uh6j.jpg">
+        Schedule
+    </Header>
+    <script src="https://CommittedBodiesFitnessCentresa.clubm.mobi/Portal/Home/clubmanager-integration.js"></script>
+<!-- svelte-ignore a11y-missing-attribute -->
+<iframe width="100%" height="1700" allowtransparency="true" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0" src="https://CommittedBodiesFitnessCentresa.clubm.mobi/Portal/Booking/Forthcoming"></iframe>
+        
+</div>
