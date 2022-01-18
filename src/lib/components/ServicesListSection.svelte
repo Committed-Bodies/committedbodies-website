e9d8b0b2329841d6fe1958c3e41ad755390351aa
Donavan Grobler <donavangrobler2@gmail.com>
2022-01-18 13:58:54 +0200

    - Services page

---
+++ src/lib/components/ServicesListSection.svelte
@@ -0,0 +1,105 @@
+<script>
+    import servicesData from "$lib/data/servicesPage.json"
+    export let services;
+</script>
+
+<section id="services">
+    <div class="content">
+        <h2>{servicesData.intro.title}</h2>
+        <p class="subtitle h4">
+            {servicesData.intro.subtitle}
+        </p>
+        <p>
+            {servicesData.intro.content}
+        </p>
+    </div>
+    <div class="servicesGallery">
+        {#each services as service}
+            {#if service.metadata.group == "Services"}
+                <div class="serviceWrap">
+                    <img src="{service.metadata.thumbnail}" alt="Cool profile pic for {service.metadata.firstName}">
+                    <h3 class="h5">{service.metadata.fullName}</h3>
+                    <ul class="prices">
+                        {#each service.metadata.prices as price}
+                            <li>{price}</li>
+                        {/each}
+                    </ul>
+                    <p>{service.metadata.blurb}</p>
+                    <a class="button" href="{`/services/${service.path.replace(".md", "")}`}" alt="Book {service.metadata.firstName}">Train with {service.metadata.firstName}</a>
+                </div>
+            {/if}
+        {/each}
+    </div>
+</section>
+
+<style lang="scss">
+    section {
+        padding: 3vw;
+        color: hsl(var(--onNeutral) / var(--onNeutralStrength0));
+        .content {
+            grid-column: 2;
+            grid-row: 1;
+            color: hsl(var(--onNeutral) / var(--onNeutralStrength0));
+            h2 {
+                margin-top: 0;
+				font-family: $font_boldEmphasis;
+				line-height: 1;
+				color: hsl(var(--onNeutral) / var(--onNeutralStrength3));
+			}
+			.subtitle {
+				font-family: $font_cursiveEmphasis;
+				margin-top: 1em;
+				color: hsl(var(--onNeutral) / var(--onNeutralStrength1));
+			}
+        }
+        .servicesGallery {
+            .serviceWrap {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-direction: column;
+                padding: 1rem 0;
+                position: relative;
+                h3 {
+                    margin: 0;
+                    color: hsl(var(--onNeutral) / var(--onNeutralStrength2));
+                }
+                p {
+                    margin: 0;
+                }
+                .prices {
+                    list-style-type: none;
+                    padding: 0;
+                    margin: 0;
+                    li {
+                        padding: 0.1rem 0.5rem;
+                        display: inline-block;
+                        margin: 3px;
+                        border-radius: 3px;
+                        font-size: 0.8rem;
+                        font-weight: 500;
+                        background: hsl(var(--neutralHS) calc(var(--neutralL) + 5%));
+                        color: hsl(var(--onNeutral) / var(--onNeutralStrength3));
+                        text-transform: uppercase;
+                        letter-spacing: 1px;
+                    }
+                }
+                .button {
+                    margin-top: 1rem;
+                    @include button;
+                    @include buttonPrimary;
+                }
+            }
+        }
+    }
+// Responsive styles md - lg screens
+@media (min-width: $mdScreen) {
+    section {
+        .servicesGallery {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-gap: 2rem;
+        }
+    }
+}
+</style>
--- src/routes/services/index.svelte
+++ src/routes/services/index.svelte
@@ -1,22 +1,47 @@
+<script context="module">
+    const allServices = import.meta.glob('./*.md')
+    let service = [];
+    for (let path in allServices) {
+
+        service.push(
+            allServices[path]().then(({ metadata }) => {
+                return {path, metadata};
+            })
+        );
+    }
+    export const load = async() => {
+        const services = await Promise.all(service);
+        return {
+            props: {
+                services,
+            }
+        }
+    };
+
+</script>
+
 <script>
     import Header from '$lib/components/Header.svelte';
+    import ServicesSection from "$lib/components/ServicesListSection.svelte";
+    export let services;
     // import { slideFadeIn, slideFadeOut } from "$lib/animation/transition-slideFade";
 </script>
 
 
 <div class="pageWrap">
-    <Header>SERVICES</Header>
-    <section>
-        <h3>PLACEHOLDER</h3>
-    </section>
+    <Header bgImage="https://res.cloudinary.com/committed-bodies/image/upload/f_auto,q_auto,t_pageHeader/v1631431623/gym/Gym-in-Benoni_m5uh6j.jpg">
+        SERVICES
+    </Header>
+
+    <ServicesSection {services} />
 </div>
 
 
 
 <style lang="scss">
-    section {
-        :global(ul) {
-            list-style-type: none;
-        }
-    }
+    // section {
+    //     :global(ul) {
+    //         list-style-type: none;
+    //     }
+    // }
 </style>
