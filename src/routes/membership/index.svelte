8e76226652a0d4d287c9f51e141e5b371b0cb1fe
sonya-ninja <hello@sonya.ninja>
2022-01-25 13:40:12 +0200

    some padding and intro component

--- src/routes/membership/index.svelte
+++ src/routes/membership/index.svelte
@@ -1,6 +1,8 @@
 <script>
-    import Header from '$lib/components/Header.svelte';
     import membershipPage from '$lib/data/membershipPage.json'
+    import Header from '$lib/components/Header.svelte';
+    let introData = membershipPage.introJoin;
+    import PageIntroSection from '$lib/components/PageIntroSection.svelte';
 </script>
 
 
@@ -8,34 +10,22 @@
     <Header bgImage="https://res.cloudinary.com/committed-bodies/image/upload/f_auto,q_auto,t_pageHeader/v1631431623/gym/Gym-in-Benoni_m5uh6j.jpg">
         Membership
     </Header>
+    <!-- See about putting this script just before the closing body tag on this page -->
     <script src="https://CommittedBodiesFitnessCentresa.clubm.mobi/Portal/Home/clubmanager-integration.js"></script>
 
     <div class="contentWrap">
         
-        <div class="content">
-            <h2>
-                {membershipPage.introJoin.title}
-            </h2>
-            <p class="subtitle h4">
-                {membershipPage.introJoin.subtitle}
-            </p>
-            <p>
-                {membershipPage.introJoin.content}
-            </p>
-        </div>
+        <PageIntroSection {introData} />
 
-<!-- svelte-ignore a11y-missing-attribute -->
-<iframe width="100%" height="1700" allowtransparency="true" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0" src="https://CommittedBodiesFitnessCentresa.clubm.mobi/Member/Joining.mvc"></iframe>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <iframe width="100%" height="1700" allowtransparency="true" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0" src="https://CommittedBodiesFitnessCentresa.clubm.mobi/Member/Joining.mvc"></iframe>
                         
     </div>
 
 </div>
 
-
-
-<!-- <style lang="scss">
-    section {
-        padding: $space2;
-        max-width: 100%;
+<style>
+    iframe {
+        padding: 3vw;
     }
-</style> -->
+</style>
