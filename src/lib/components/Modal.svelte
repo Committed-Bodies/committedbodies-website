dfc02f9f15c12d33bfcd656351be10bb9aa71f46
sonya-ninja <hello@sonya.ninja>
2022-02-09 20:55:08 +0200

    modal padding

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -39,7 +39,7 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        padding: 0;
+        padding: 3rem;
         .modalWrap {
             position: relative;
             width: 100%;
